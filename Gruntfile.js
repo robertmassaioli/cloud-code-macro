e36ae560cb0068ce269222376ac676d02785a74d
Robert Massaioli <rmassaioli@atlassian.com>
2016-03-10 11:23:29 +1100

    The node express server is working with logging.
    Now is the time to start building out the features. We should create the first macros.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,17 @@
 module.exports = function(grunt) {
+   var expressRoot = 'index.js';
 
    grunt.initConfig({
+      express: {
+         options: {
+            // Override defaults here
+         },
+         dev: {
+            options: {
+               script: expressRoot
+            }
+         }
+      },
       jshint: {
          files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
          options: {
@@ -10,8 +21,32 @@ module.exports = function(grunt) {
          }
       },
       watch: {
-         files: ['<%= jshint.files %>'],
-         tasks: ['jshint']
+         express: {
+            files: [ 
+               'Gruntfile.js',
+               expressRoot,
+               'views/*.mustache'
+            ],
+            tasks: [ 'express:dev' ],
+            options: {
+               spawn: false
+            }
+         },
+         requirejs: {
+            files: [ 
+               'Gruntfile.js',
+               'static/js/**/*.js' 
+            ],
+            tasks: [ 'requirejs:compile' ]
+         },
+         less: {
+            files: [ 'static/less/**/*.less' ],
+            tasks: [ 'less:compile' ]
+         },
+         jshint: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint']
+         }
       }
    });
 
@@ -21,5 +56,10 @@ module.exports = function(grunt) {
    grunt.loadNpmTasks('grunt-contrib-jshint');
    grunt.loadNpmTasks('grunt-contrib-watch');
 
-   grunt.registerTask('default', ['jshint']);
+   grunt.registerTask('default', [
+      //'requirejs:compile', 
+      //'less:compile', 
+      'express:dev', 
+      'watch'
+   ]);
 };
--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 var express = require('express');
+var bunyan = require('express-bunyan-logger');
 var app = express();
+
+// JSON Logging
+app.use(bunyan());
+app.use(bunyan.errorLogger());
+
+// Variables for setting up this addon
+var serverPort = process.env.PORT || 3000;
+var hostUrl = process.env.HOST_URL || "http://localhost:" + serverPort;
+
+// Register static variables
+app.use('/static/images', express.static('static/images'));
+app.use('/static/js', express.static('static-js'));
+app.use('/static/css', express.static('static-css'));
+app.use('/static/ace', express.static('static/ace'));
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
   res.send('hello world');
+});
+
+var server = app.listen(serverPort, function () {
+   var host = server.address().address;
+   var port = server.address().port;
+
+   console.log('Example app listening at http://%s:%s', host, port);
 });
