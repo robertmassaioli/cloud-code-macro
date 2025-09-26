62b7e4d7ecd72bc9ecd1b1b1089f5b3dce80b390
Developer <developer@atlassian.com>
2025-09-26 13:40:33 +1000

    Convert Connect backend to static HTML deprecation views
    - Convert all .mustache template files to .html static files
    - Remove mustache-express dependency and templating engine setup
    - Update all /macro/ routes to serve static HTML files directly
    - Remove unused lodash, sanitize-filename dependencies
    - Clean up prettyStyleName function and avaliableStyles array
    - Simplify docs route to serve static deprecation message
    - Remove app.param middleware and dynamic template rendering
    - Add connect-cdn.atl-paas.net/all.js script to all HTML views for proper Connect integration
    
    All macro routes now serve static deprecation messages encouraging users
    to contact admins for Forge migration. Backend is fully static and simplified.

--- remotes/connect/index.js
+++ remotes/connect/index.js
@@ -1,9 +1,7 @@
 var express = require('express');
-var _ = require('lodash');
 var ExpressPinoLogger = require('express-pino-logger');
-var mustacheExpress = require('mustache-express');
 const fs = require('fs');
-const sanitize = require("sanitize-filename");
+const path = require('path');
 var app = express();
 
 // JSON Logging
@@ -20,10 +18,7 @@ const pino = ExpressPinoLogger({
 
 app.use(pino);
 
-// Register '.mustache' extension with The Mustache Express
-app.engine('mustache', mustacheExpress());
-app.set('view engine', 'mustache');
-app.set('views', __dirname + '/views');
+// Static HTML views - no template engine needed
 
 // Variables for setting up this addon
 var serverPort = process.env.PORT || 3000;
@@ -81,359 +76,33 @@ app.get('/', function(req, res) {
 
 var pluginKey = 'com.atlassian.connect.better-code-macro' + getKeySuffixFromZone(microsZone);
 
-app.get('/atlassian-connect.json', function(req, res) {
-   res.json({
-      name: 'Better Code Macro for Confluence',
-      key: pluginKey,
-      version: "1.1",
-      description: 'A better code macro for Confluence cloud.',
-      vendor: {
-         name: 'Atlassian',
-         url: 'http://www.atlassian.com'
-      },
-      baseUrl: hostUrl,
-      authentication: {
-         type: "none"
-      },
-      apiMigrations: {
-        gdpr: true
-      },
-      scopes: ["read"],
-      modules: {
-         dynamicContentMacros: [{
-            key: 'bitbucket-snippet-code-macro',
-            name: {
-               value: 'Bitbucket snippet macro'
-            },
-            url: '/macro/bitbucket-snippet-code-macro?snippetUrl={snippetUrl}',
-            icon: {
-               width: 80,
-               height: 80,
-               url: '/static/images/bitbucket/bitbucket-logo-80px.png'
-            },
-            documentation: {
-               url: '/docs/bitbucket-snippets'
-            },
-            categories: [
-               'development',
-               'external-content'
-            ],
-            outputType: 'block',
-            bodyType: 'none',
-            aliases: [
-               'snippet',
-               'bbsnippet',
-               'bitbucket',
-            ],
-            featured: false,
-            parameters: [{
-               identifier: 'snippetUrl',
-               name: {
-                  value: 'Snippet url'
-               },
-               type: 'string',
-               required: true,
-               multiple: false,
-               hidden: true
-            }],
-            autoconvert: {
-               urlParameter: 'snippetUrl',
-               matchers: [{
-                  pattern: 'https://bitbucket.org/snippets/{}/{}'
-               }, {
-                  pattern: 'https://bitbucket.org/snippets/{}/{}/{}'
-               }]
-            },
-            hidden: true
-         }, {
-            key: 'gist-code-macro',
-            name: {
-               value: 'GitHub gist macro'
-            },
-            url: '/macro/gist-code-macro?gistUrl={gistUrl}',
-            icon: {
-               width: 80,
-               height: 80,
-               url: '/static/images/github/GitHub-Mark-80px.png'
-            },
-            documentation: {
-               url: '/docs/gist-code-macro'
-            },
-            categories: [
-               'development',
-               'external-content'
-            ],
-            outputType: 'block',
-            bodyType: 'none',
-            aliases: [ 'gist' ],
-            featured: false,
-            parameters: [{
-               identifier: 'gistUrl',
-               name: {
-                  value: 'Gist url'
-               },
-               type: 'string',
-               required: true,
-               multiple: false,
-               hidden: true
-            }],
-            autoconvert: {
-               urlParameter: 'gistUrl',
-               matchers: [{
-                  pattern: 'https://gist.github.com/{}/{}'
-               }]
-            },
-            hidden: true
-         }, {
-            key: 'paste-code-macro',
-            name: {
-               value: 'Better Code Block'
-            },
-            url: '/macro/paste-code-macro?page_id={page.id}&macro_id={macro.id}&page_version={page.version}&output_type={output.type}&theme={theme}&language={language}&title={title}',
-            description: {
-               value: 'Better macro to format blocks of source code or markup.'
-            },
-            icon: {
-               width: 80,
-               height: 80,
-               url: '/static/images/cloud-code-macro-paste-icon.png'
-            },
-            documentation: {
-               url: '/docs/paste-code-macro'
-            },
-            // TODO use external content for the snippets and gist macros
-            categories: [
-               'development',
-               'formatting'
-            ],
-            outputType: 'block',
-            bodyType: 'plain-text',
-            aliases: [
-               'bettercode',
-               'codebetter',
-               'bcode'
-            ],
-            featured: true,
-            parameters: [{
-               identifier: 'language',
-               name: {
-                  value: 'Language'
-               },
-               description: {
-                  value: 'Choose the programming / markup language for your code explicitly.'
-               },
-               type: 'enum',
-               required: false,
-               // These values were generated by cd'ing into the highlight.js/src/language directory
-               // and running: ls -1 | sed 's_.js_",_' | sed 's_^_"_' | pbcopy
-               values: [
-                  "1c",
-                  "accesslog",
-                  "actionscript",
-                  "apache",
-                  "applescript",
-                  "arduino",
-                  "armasm",
-                  "asciidoc",
-                  "aspectj",
-                  "autohotkey",
-                  "autoit",
-                  "avrasm",
-                  "axapta",
-                  "bash",
-                  "basic",
-                  "brainfuck",
-                  "cal",
-                  "capnproto",
-                  "ceylon",
-                  "clojure-repl",
-                  "clojure",
-                  "cmake",
-                  "coffeescript",
-                  "cos",
-                  "cpp",
-                  "crmsh",
-                  "crystal",
-                  "cs",
-                  "csp",
-                  "css",
-                  "d",
-                  "dart",
-                  "delphi",
-                  "diff",
-                  "django",
-                  "dns",
-                  "dockerfile",
-                  "dos",
-                  "dts",
-                  "dust",
-                  "elixir",
-                  "elm",
-                  "erb",
-                  "erlang-repl",
-                  "erlang",
-                  "fix",
-                  "fortran",
-                  "fsharp",
-                  "gams",
-                  "gauss",
-                  "gcode",
-                  "gherkin",
-                  "glsl",
-                  "go",
-                  "golo",
-                  "gradle",
-                  "groovy",
-                  "haml",
-                  "handlebars",
-                  "haskell",
-                  "haxe",
-                  "hsp",
-                  "htmlbars",
-                  "http",
-                  "inform7",
-                  "ini",
-                  "irpf90",
-                  "java",
-                  "javascript",
-                  "json",
-                  "julia",
-                  "kotlin",
-                  "lasso",
-                  "less",
-                  "lisp",
-                  "livecodeserver",
-                  "livescript",
-                  "lua",
-                  "makefile",
-                  "markdown",
-                  "mathematica",
-                  "matlab",
-                  "maxima",
-                  "mel",
-                  "mercury",
-                  "mipsasm",
-                  "mizar",
-                  "mojolicious",
-                  "monkey",
-                  "nginx",
-                  "nimrod",
-                  "nix",
-                  "nsis",
-                  "objectivec",
-                  "ocaml",
-                  "openscad",
-                  "oxygene",
-                  "parser3",
-                  "perl",
-                  "pf",
-                  "php",
-                  "powershell",
-                  "processing",
-                  "profile",
-                  "prolog",
-                  "protobuf",
-                  "puppet",
-                  "python",
-                  "q",
-                  "qml",
-                  "r",
-                  "rib",
-                  "roboconf",
-                  "rsl",
-                  "ruby",
-                  "ruleslanguage",
-                  "rust",
-                  "scala",
-                  "scheme",
-                  "scilab",
-                  "scss",
-                  "smali",
-                  "smalltalk",
-                  "sml",
-                  "sqf",
-                  "sql",
-                  "stan",
-                  "stata",
-                  "step21",
-                  "stylus",
-                  "swift",
-                  "taggerscript",
-                  "tcl",
-                  "tex",
-                  "thrift",
-                  "tp",
-                  "twig",
-                  "typescript",
-                  "vala",
-                  "vbnet",
-                  "vbscript-html",
-                  "vbscript",
-                  "verilog",
-                  "vhdl",
-                  "vim",
-                  "x86asm",
-                  "xl",
-                  "xml",
-                  "xquery",
-                  "yaml",
-                  "zephir"
-               ],
-               hidden: false
-            }, {
-               identifier: 'title',
-               name: {
-                  value: 'Title'
-               },
-               description: {
-                  value: 'An optional title for your code black.'
-               },
-               type: 'string',
-               required: false,
-            }, {
-               identifier: 'theme',
-               name: {
-                  value: 'Theme'
-               },
-               description: {
-                  value: 'Choose the theme for your code.'
-               },
-               type: 'enum',
-               required: false,
-               values: _.map(avaliableStyles, prettyStyleName),
-               defaultValue: prettyStyleName('github-gist')
-            }],
-         }]
+app.get('/macro/paste-code-macro', function(req, res) {
+   fs.readFile(path.join(__dirname, 'views', 'paste-code-macro.html'), 'utf8', function(err, data) {
+      if (err) {
+         res.status(500).send('Error loading page');
+      } else {
+         res.send(data);
       }
    });
 });
 
-app.get('/macro/paste-code-macro', function(req, res) {
-   var toTheme = function(s) { return { theme: s }; };
-   res.render('paste-code-macro', {
-      avaliableStyles: _.map(avaliableStyles, toTheme)
-   });
-});
-
 app.get('/macro/gist-code-macro', function(req, res) {
-   var gistUrl = new URL(req.query.gistUrl || '');
-   if (gistUrl.protocol === 'https:' && gistUrl.hostname !== 'gist.github.com') {
-      gistUrl = 'https://gist.github.com/robertmassaioli/ef414358f703318cf24616513aad185b';
-   }
-
-   res.render('gist-code-macro', {
-       gistUrl: gistUrl + '.js'
+   fs.readFile(path.join(__dirname, 'views', 'gist-code-macro.html'), 'utf8', function(err, data) {
+      if (err) {
+         res.status(500).send('Error loading page');
+      } else {
+         res.send(data);
+      }
    });
 });
 
 app.get('/macro/bitbucket-snippet-code-macro', function(req, res) {
-   var snippetUrl = new URL(req.query.snippetUrl || '');
-   if (snippetUrl.protocol === 'https:' && snippetUrl.hostname !== 'bitbucket.org') {
-      snippetUrl = 'https://bitbucket.org/snippets/atlassian/AeBxKn';
-   }
-
-   res.render('bitbucket-snippet-code-macro', {
-       snippetUrl: snippetUrl
+   fs.readFile(path.join(__dirname, 'views', 'bitbucket-snippet-code-macro.html'), 'utf8', function(err, data) {
+      if (err) {
+         res.status(500).send('Error loading page');
+      } else {
+         res.send(data);
+      }
    });
 });
 
@@ -454,125 +123,16 @@ app.get('/redirect/raise-issue', function(req, res) {
    res.redirect('https://bitbucket.org/robertmassaioli/cloud-code-macro/issues/new');
 });
 
-app.param('docsFile', function(req, res, next, docsFile) {
-   req.docsFile = sanitize(docsFile);
-   next();
-});
-
 app.get('/docs/:docsFile', function(req, res) {
-   fs.readFile('docs/' + req.docsFile + '.md', function(err, data) {
-      if(err) {
-         fs.readFile('docs/not-found.md', function(err, data) {
-            if(err) {
-               res.sendStatus(404);
-            } else {
-               res.render('docs', {
-                  markdownContent: data
-               });
-            }
-         });
+   fs.readFile(path.join(__dirname, 'views', 'docs.html'), 'utf8', function(err, data) {
+      if (err) {
+         res.status(500).send('Error loading page');
       } else {
-         res.render('docs', {
-            markdownContent: data
-         });
+         res.send(data);
       }
    });
 });
 
-var prettyStyleName = function(s) {
-   var upperLocations = [];
-   var currentLocation = 0;
-   while(currentLocation >= 0) {
-      currentLocation = s.indexOf('-', currentLocation + 1);
-      if(currentLocation > 0) upperLocations.push(currentLocation);
-   }
-
-   var finalName = s[0].toUpperCase();
-   var lastPos = 1;
-   for(var i = 0; i < upperLocations.length; ++i) {
-      finalName += s.substring(lastPos, upperLocations[i]) + ' ' + s[upperLocations[i] + 1].toUpperCase();
-      lastPos = upperLocations[i] + 2;
-   }
-   finalName += s.substring(lastPos);
-
-   return finalName;
-};
-
-// These values were generated by cd'ing into the highlight.js/src/styles directory
-// and running: ls -1 *.css | sed 's_.css_",_' | sed 's_^_"_' | pbcopy
-var avaliableStyles = [
-   "agate",
-   "androidstudio",
-   "arduino-light",
-   "arta",
-   "ascetic",
-   "atelier-cave-dark",
-   "atelier-cave-light",
-   "atelier-dune-dark",
-   "atelier-dune-light",
-   "atelier-estuary-dark",
-   "atelier-estuary-light",
-   "atelier-forest-dark",
-   "atelier-forest-light",
-   "atelier-heath-dark",
-   "atelier-heath-light",
-   "atelier-lakeside-dark",
-   "atelier-lakeside-light",
-   "atelier-plateau-dark",
-   "atelier-plateau-light",
-   "atelier-savanna-dark",
-   "atelier-savanna-light",
-   "atelier-seaside-dark",
-   "atelier-seaside-light",
-   "atelier-sulphurpool-dark",
-   "atelier-sulphurpool-light",
-   "brown-paper",
-   "codepen-embed",
-   "color-brewer",
-   "dark",
-   "darkula",
-   "default",
-   "docco",
-   "dracula",
-   "far",
-   "foundation",
-   "github-gist",
-   "github",
-   "googlecode",
-   "grayscale",
-   "gruvbox-dark",
-   "gruvbox-light",
-   "hopscotch",
-   "hybrid",
-   "idea",
-   "ir-black",
-   "kimbie.dark",
-   "kimbie.light",
-   "magula",
-   "mono-blue",
-   "monokai-sublime",
-   "monokai",
-   "obsidian",
-   "paraiso-dark",
-   "paraiso-light",
-   "pojoaque",
-   "qtcreator_dark",
-   "qtcreator_light",
-   "railscasts",
-   "rainbow",
-   "school-book",
-   "solarized-dark",
-   "solarized-light",
-   "sunburst",
-   "tomorrow-night-blue",
-   "tomorrow-night-bright",
-   "tomorrow-night-eighties",
-   "tomorrow-night",
-   "tomorrow",
-   "vs",
-   "xcode",
-   "zenburn"
-];
 
 var server = app.listen(serverPort, function () {
    var host = server.address().address;
